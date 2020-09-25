001d2c7199bc4c396135fc3db739d4dd2ef3d7ac
Marvin Micael Pacia <vinnopacia@gmail.com>
2020-09-25 01:18:00 -0700

    Update beer menu

--- inc/beer-menu.php
+++ inc/beer-menu.php
@@ -4,7 +4,6 @@ function beer_menu()
 {
   return $menu = [
     [
-      [
         'title' => 'Snack & Share',
         'menu' => [
           [
@@ -50,98 +49,95 @@ function beer_menu()
             'desc' => 'kaffir lime dressing, red pepper, gochugaru panko crumb',
             'price' => 14,
           ],
-        ]
-      ],
-      [
-        'title' => 'Wings',
-        'menu' => [
-          [
-            'title' => 'Spicy Rub',
-            'gf' => true,
-            'desc' => 'dried chilis, garlic, and sugar, served with buttermilk ranch',
-            'price' => 14,
-          ],
-          [
-            'title' => 'Herb Rub',
-            'gf' => true,
-            'desc' => 'dried rosemary, thyme, and sage, served with buttermilk ranch',
-            'price' => 14,
-          ],
-          [
-            'title' => 'Barbecue Rub',
-            'gf' => true,
-            'desc' => 'brown sugar, paprika, lapsang, served with buttermilk ranch',
-            'price' => 14,
-          ],
-          [
-            'title' => 'Lemon Pepper Honey',
-            'gf' => true,
-            'desc' => 'lemon pepper rub with apple cider honey glaze',
-            'price' => 14,
-          ],
-          [
-            'title' => 'Yuzu Fish Sauce',
-            'gf' => true,
-            'desc' => 'chopped garlic, thai chili, and green onion, with yuzu fish sauce glaze',
-            'price' => 14,
-          ],
-          [
-            'title' => 'Salt and Pepper',
-            'gf' => true,
-            'price' => 14,
-          ],
+      ]
+    ],
+    [
+      'title' => 'Wings',
+      'menu' => [
+        [
+          'title' => 'Spicy Rub',
+          '' => true,
+          'desc' => 'dried chilis, garlic, and sugar, served with buttermilk ranch',
+          'price' => 20,
+        ],
+        [
+          'title' => 'Herb Rub',
+          'gf' => true,
+          'desc' => 'dried rosemary, thyme, and sage, served with buttermilk ranch',
+          'price' => 14,
+        ],
+        [
+          'title' => 'Barbecue Rub',
+          'gf' => true,
+          'desc' => 'brown sugar, paprika, lapsang, served with buttermilk ranch',
+          'price' => 14,
+        ],
+        [
+          'title' => 'Lemon Pepper Honey',
+          'gf' => true,
+          'desc' => 'lemon pepper rub with apple cider honey glaze',
+          'price' => 14,
+        ],
+        [
+          'title' => 'Yuzu Fish Sauce',
+          'gf' => true,
+          'desc' => 'chopped garlic, thai chili, and green onion, with yuzu fish sauce glaze',
+          'price' => 14,
+        ],
+        [
+          'title' => 'Salt and Pepper',
+          'gf' => true,
+          'price' => 14,
         ],
       ],
     ],
     [
-      [
-        'title' => 'Burgers',
-        'desc' => 'Add a side of Mixed Greens, Buttermilk Slaw, Caesar Salad, Duck Fat, Fries, or Waffle Fries, $3',
-        'menu' => [
-          [
-            'title' => 'Spicy Chicken Sandwich',
-            'desc' => 'brioche, slaw, valentina mayo, pickled jalapeno',
-            'price' => 13,
-          ],
-          [
-            'title' => 'Classic Chicken Sandwich',
-            'desc' => 'brioche, mayo, iceberg',
-            'price' => 10,
-          ],
-          [
-            'title' => 'Korean Chicken Sandwich',
-            'desc' => 'brioche, kimchi slaw, gochujang, sesame, green onion',
-            'price' => 13,
-          ],
-          [
-            'title' => 'Beef Burger',
-            'desc' => 'brioche, sauce, onion, pickled cucumber, cheddar, iceberg',
-            'price' => 13,
-          ],
-          [
-            'title' => 'Veggie Burger',
-            'desc' => 'brioche, sauce, onion, pickled cucumber, cheddar, iceberg',
-            'price' => 13,
-          ],
+      'title' => 'Burgers',
+      'desc' => 'Add a side of Mixed Greens, Buttermilk Slaw, Caesar Salad, Duck Fat, Fries, or Waffle Fries, $3',
+      'menu' => [
+        [
+          'title' => 'Spicy Chicken Sandwich',
+          'desc' => 'brioche, slaw, valentina mayo, pickled jalapeno',
+          'price' => 13,
+        ],
+        [
+          'title' => 'Classic Chicken Sandwich',
+          'desc' => 'brioche, mayo, iceberg',
+          'price' => 10,
+        ],
+        [
+          'title' => 'Korean Chicken Sandwich',
+          'desc' => 'brioche, kimchi slaw, gochujang, sesame, green onion',
+          'price' => 13,
+        ],
+        [
+          'title' => 'Beef Burger',
+          'desc' => 'brioche, sauce, onion, pickled cucumber, cheddar, iceberg',
+          'price' => 13,
+        ],
+        [
+          'title' => 'Veggie Burger',
+          'desc' => 'brioche, sauce, onion, pickled cucumber, cheddar, iceberg',
+          'price' => 13,
         ],
       ],
-      [
-        'title' => 'Wraps',
-        'desc' => 'Add a side of Mixed Greens, Buttermilk Slaw, Caesar Salad, Duck Fat Fries, or Waffle Fries, $3',
-        'menu' => [
-          [
-            'title' => 'Falafel Wrap',
-            'v' => true,
-            'desc' => 'flour tortilla, cilantro harissa tahini, cucumber, tomato, pickled banana pepper, iceberg',
-            'price' => 13,
-          ],
-          [
-            'title' => 'Chicken Ceasar Wrap',
-            'desc' => 'flour tortilla, salt & pepper fried chicken, romaine, tomato, parmesan',
-            'price' => 13,
-          ],
+    ],
+    [
+      'title' => 'Wraps',
+      'desc' => 'Add a side of Mixed Greens, Buttermilk Slaw, Caesar Salad, Duck Fat Fries, or Waffle Fries, $3',
+      'menu' => [
+        [
+          'title' => 'Falafel Wrap',
+          'v' => true,
+          'desc' => 'flour tortilla, cilantro harissa tahini, cucumber, tomato, pickled banana pepper, iceberg',
+          'price' => 13,
+        ],
+        [
+          'title' => 'Chicken Ceasar Wrap',
+          'desc' => 'flour tortilla, salt & pepper fried chicken, romaine, tomato, parmesan',
+          'price' => 13,
         ],
       ],
-    ]
+    ],
   ];
 }
