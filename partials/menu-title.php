4b5487bbe21950eedd719fadc3bdc04a2f6e01d7
Ynah Pantig <me@ynahpantig.com>
2020-09-28 01:19:11 -0700

    Fix menu title

--- partials/menu-title.php
+++ partials/menu-title.php
@@ -1,4 +1,4 @@
-<div class="container page-title pt-3 pt-mb-0">
+<div class="container page-title pt-3 pt-md-0">
   <div class="row">
     <div class="col-12 col-md-10 text-md-right page-title-text px-0">
       <h1 class="font-weight-bold text-uppercase"><?php echo $page_title; ?></h1>
